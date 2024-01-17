eaffbe6d4838f7e8eebe98bf035952f44c0c9e27
Juan <108071080+JuanIgA@users.noreply.github.com>
2024-01-17 16:36:28 -0300

    Add files via upload
    Programa donde se ingresa nombre y sexo con una condicional. / Program where inscript to name and sex are entered with a condicional.

---
+++ Nombre-sexo.py
@@ -0,0 +1,9 @@
+usuario = input("Ingrese su nombre: ")
+sexo = input("Ingrese su sexo: ")
+
+if sexo == "femenino":
+    print("Bienvenida ", usuario)
+    print("Ahora estás en la Mátrix")
+else:
+    print("Bienvenido ", usuario)
+    print("Ahora estás en la Mátrix")
